5665d230f6943514925734c8a1aee80ce45da636
Sheethala Swaminathan <sheethala@Sheethalas-MacBook-Pro.local>
2016-03-02 19:47:36 -0500

    Added style sheets
    added new fonts

--- javascripts/ratemyroof.js
+++ javascripts/ratemyroof.js
@@ -1,1 +1,15 @@
 
+var myFirebaseRef = new Firebase("https://ratemyroof.firebaseio.com/");
+myFirebaseRef.set({
+  title: "Hello World!",
+  author: "Firebase",
+  location: {
+    city: "San Francisco",
+    state: "California",
+    zip: 94103
+  }
+});
+
+myFirebaseRef.child("location/city").on("value", function(snapshot) {
+  alert(snapshot.val());  // Alerts "San Francisco"
+});
