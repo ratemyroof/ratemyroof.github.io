696782a73507911d345be171731eda0304b315b7
Sheethala Swaminathan <sheethala@Sheethalas-MacBook-Pro.local>
2016-03-13 19:44:56 -0400

    added ratemyroof.html
    -Created a function where users can add a house

--- javascripts/ratemyroof.js
+++ javascripts/ratemyroof.js
@@ -1,20 +1,23 @@
-<<<<<<< HEAD
 
 var myFirebaseRef = new Firebase("https://ratemyroof.firebaseio.com/");
-myFirebaseRef.set({
-  title: "Hello World!",
-  author: "Firebase",
-  location: {
-    city: "San Francisco",
-    state: "California",
-    zip: 94103
-  }
-});
 
-myFirebaseRef.child("location/city").on("value", function(snapshot) {
-  alert(snapshot.val());  // Alerts "San Francisco"
-});
-=======
-var Firebase = require("firebase");
-var rootRef = new Firebase('https://resplendent-inferno-8914.firebaseio.com/');
->>>>>>> origin/master
+function createProperty(){
+  var street_number = document.getElementById("street_number");
+  var street_name = document.getElementById("street_name");
+  console.log(street_number);
+  console.log(street_name);
+  var property_name = street_number.value + street_name.value
+  myFirebaseRef.push({
+    'streetNumber': street_number.value,
+    'streetName': street_name.value,
+    'location': {
+      city: "San Francisco",
+      state: "California",
+      zip: 94103
+    }
+  });
+  myFirebaseRef.child("location/city").on("value", function(snapshot) {
+    alert(snapshot.val());  // Alerts "San Francisco"
+  });
+  alert('saved')
+}
